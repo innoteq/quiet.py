3d30089cc5105424bba3a93f5ba8f5b59a0e2f2c
Yihui Xiong <yihui.xiong@hotmail.com>
2018-05-04 19:46:18 +0800

    without fftw3 and portaudio, generate shared library from static libraries

--- scripts/libs.sh
+++ scripts/libs.sh
@@ -23,19 +23,19 @@ rm -f "$SYSROOT/usr/lib/libfec.dylib"
 
 mkdir -p "$BUILDPATH/liquid-dsp"
 cd "$BUILDPATH/liquid-dsp"
-cmake -DCMAKE_BUILD_TYPE=Release "$SRCPATH/liquid-dsp" -DCMAKE_INSTALL_PREFIX="$SYSROOT/usr" -DCMAKE_PREFIX_PATH="$SYSROOT" -DCMAKE_SHARED_LINKER_FLAGS="-L$SYSROOT/usr/lib" -DLIQUID_BUILD_EXAMPLES="off" -DLIQUID_BUILD_SANDBOX="off" && make liquid-static && make install
+cmake -DCMAKE_BUILD_TYPE=Release "$SRCPATH/liquid-dsp" -DCMAKE_C_FLAGS="-fPIC" -DLIQUID_FFTOVERRIDE=ON -DCMAKE_INSTALL_PREFIX="$SYSROOT/usr" -DCMAKE_PREFIX_PATH="$SYSROOT" -DCMAKE_SHARED_LINKER_FLAGS="-L$SYSROOT/usr/lib" -DLIQUID_BUILD_EXAMPLES="off" -DLIQUID_BUILD_SANDBOX="off" && make liquid-static && make install
 
 mkdir -p "$BUILDPATH/jansson"
 cd "$BUILDPATH/jansson"
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$SYSROOT/usr" -DJANSSON_BUILD_SHARED_LIBS=off -DJANSSON_WITHOUT_TESTS=on -DJANSSON_EXAMPLES=off -DJANSSON_BUILD_DOCS=off "$SRCPATH/jansson" && make && make install
 
-mkdir -p "$BUILDPATH/portaudio"
-cd "$BUILDPATH/portaudio"
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$SYSROOT/usr" -DCMAKE_PREFIX_PATH="$SYSROOT" "$SRCPATH/portaudio" && make && make install && cp libportaudio_static.a "$SYSROOT/usr/lib/libportaudio.a"
+# mkdir -p "$BUILDPATH/portaudio"
+# cd "$BUILDPATH/portaudio"
+# cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$SYSROOT/usr" -DCMAKE_PREFIX_PATH="$SYSROOT" "$SRCPATH/portaudio" && make && make install && cp libportaudio_static.a "$SYSROOT/usr/lib/libportaudio.a"
 
 mkdir -p "$BUILDPATH/libquiet"
 cd "$BUILDPATH/libquiet"
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$SYSROOT/usr" -DCMAKE_PREFIX_PATH="$SYSROOT" "$SRCPATH/libquiet" && make
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX="$SYSROOT/usr" -DCMAKE_PREFIX_PATH="$SYSROOT" "$SRCPATH/libquiet" && make
 CTEST_OUTPUT_ON_FAILURE=1 make check
 make install
 
@@ -44,14 +44,18 @@ mkdir -p "$INCLUDEPATH"
 cp "$SYSROOTPATH/usr/lib/libfec.a" "$LIBPATH"
 cp "$SYSROOTPATH/usr/lib/libliquid.a" "$LIBPATH"
 cp "$SYSROOTPATH/usr/lib/libjansson.a" "$LIBPATH"
-cp "$SYSROOTPATH/usr/lib/libportaudio.a" "$LIBPATH"
+# cp "$SYSROOTPATH/usr/lib/libportaudio.a" "$LIBPATH"
 cp "$SYSROOTPATH/usr/lib/libquiet.a" "$LIBPATH"
 cp "$SYSROOTPATH/usr/include/fec.h" "$INCLUDEPATH"
 cp -R "$SYSROOTPATH/usr/include/liquid" "$INCLUDEPATH"
 cp "$SYSROOTPATH/usr/include/jansson.h" "$INCLUDEPATH"
 cp "$SYSROOTPATH/usr/include/jansson_config.h" "$INCLUDEPATH"
-cp "$SYSROOTPATH/usr/include/portaudio.h" "$INCLUDEPATH"
+# cp "$SYSROOTPATH/usr/include/portaudio.h" "$INCLUDEPATH"
 cp "$SYSROOTPATH/usr/include/quiet.h" "$INCLUDEPATH"
+
+gcc -shared -o $LIBPATH/libquiet.so \
+-Wl,--whole-archive $LIBPATH/libquiet.a $LIBPATH/libliquid.a $LIBPATH/libfec.a \
+$LIBPATH/libjansson.a -Wl,--no-whole-archive
 
 echo
 echo "Build complete. Built libraries are in $LIBPATH"
