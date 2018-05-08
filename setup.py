9b89a635a4d8d39ff42894ee83d319087f94db77
Yihui Xiong <yihui.xiong@hotmail.com>
2018-05-08 18:12:16 +0800

    use setuptools and customize build command

--- setup.py
+++ setup.py
@@ -1,21 +1,35 @@
-from distutils.core import setup, Extension
 
-# if osx
+
+from setuptools import setup
+from setuptools.command.build_py import build_py
+import ctypes.util
+import subprocess
 import os
-os.environ['LDFLAGS'] = '-framework Carbon -framework AudioUnit -framework AudioToolbox -framework CoreAudio'
 
-module = Extension('cquiet',
-                   sources=['quietmodule.c'],
-                   extra_compile_args=['-std=c99', '-Iinclude'],
-                   extra_link_args=['-Llib', 'libquiet.a', 'libportaudio.a', 'libjansson.a', 'libliquid.a', 'libfec.a'],
-                   )
+
+class BuildPyCommand(build_py):
+    """Custom build command."""
+
+    def run(self):
+        # check if libquiet.so is at system lib paths
+        if not ctypes.util.find_library('quiet'):
+            libquiet = os.path.join(os.path.dirname(
+                __file__), 'quiet', 'libquiet.so')
+            if not os.path.isfile(libquiet):
+                # build libquiet.so
+                subprocess.check_call(['bash', 'scripts/libs.sh'])
+
+        build_py.run(self)
+
 
 setup(name='quiet',
       version='0.1',
       description='Quiet Modem',
-      author='Brian Armstrong',
+      author='Brian Armstrong, Yihui Xiong',
       author_email='brian.armstrong.ece+pypi@gmail.com',
-      url='https://github.com/quiet',
-      ext_modules=[module],
+      url='https://github.com/quiet/quiet.py',
+      cmdclass={
+          'build_py': BuildPyCommand,
+      },
       packages=['quiet'],
-)
+      zip_safe=False)
